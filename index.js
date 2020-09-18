1b311b6a16268d91b163910fca92a40374371825
Oyeleke Okiki <oyelekeokiki@yahoo.com>
2020-09-18 20:57:22 +0100

    Completed iOS config setup

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
-import { registerRootComponent } from 'expo';
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
-import App from './App';
+class RNHome extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.highScoresTitle}>
+          Test Data for RN Home
+        </Text>
+        <Text style={styles.scores}>{contents}</Text>
+      </View>
+    );
+  }
+}
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in the Expo client or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF'
+  },
+  highScoresTitle: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
+  scores: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5
+  }
+});
+
+// Module name
+AppRegistry.registerComponent('RNHome', () => RNHome);
--- ios/ClothesStore/App/AnimatedTabBarController.swift
+++ ios/ClothesStore/App/AnimatedTabBarController.swift
@@ -6,14 +6,25 @@
 //
 
 import UIKit
+import React
 
 class AnimatedTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let vc1: UIViewController = HomeController()
+        let jsCodeLocation = URL(string: "http://localhost:8081/index.bundle?platform=ios")!
+
+        let rootView = RCTRootView(
+            bundleURL: jsCodeLocation,
+            moduleName: "RNHome",
+            initialProperties: nil,
+            launchOptions: nil
+        )
+        let vc1 = UIViewController()
         vc1.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house.fill"), tag: 0)
+        vc1.view = rootView
+
 
         let vc2: UIViewController = WishListController()
         vc2.tabBarItem = UITabBarItem(title: "My Wishlist", image: UIImage(systemName: "star.fill"), tag: 1)
