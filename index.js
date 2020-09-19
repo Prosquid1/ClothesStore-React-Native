7a1de348b949f7b8f4f139d5813d07be2636d4e9
Oyeleke Okiki <oyelekeokiki@yahoo.com>
2020-09-19 10:55:06 +0100

    First working version of adding to wishlist

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ import {
     AppRegistry,
     StyleSheet,
     Text,
+    Alert,
+    TouchableOpacity,
     View
 } from 'react-native';
 
@@ -24,16 +26,23 @@ const RNHome = () => {
         HomeBridge
     ]);
 
+    const onAddToWishListPressed = useCallback((product) => HomeBridge.addToWishList(product), [
+        HomeBridge
+    ]);
+
+    const onRemoveFromWishListPressed = useCallback((product) => HomeBridge.removeFromWishList(product), [
+        HomeBridge
+    ]);
+
 
     useEffect(() => {
         eventEmitter.addListener('onSuccess', newProducts => {
-            console.log(`New products ${newProducts}`);
             setRefreshing(false)
             setProducts(newProducts)
         });
         eventEmitter.addListener('onError', errorMessage => {
             setRefreshing(false)
-            console.log(errorMessage);
+            Alert.alert("Error", errorMessage)
         });
 
         setRefreshing(true)
@@ -45,11 +54,18 @@ const RNHome = () => {
         <View style={styles.line}>
             <View style={styles.image}/>
             <View style={styles.textContainer}>
-                <Text style={styles.productName}>{item.name}</Text>
-                <Text style={styles.productName}>{item.category}</Text>
+                <Text style={styles.productName}>{item.name}{item.name}{item.name}{item.name}{item.name}{item.name}{item.name}</Text>
+                <Text style={styles.productName}>{item.category}{item.name}{item.name}{item.name}{item.name}{item.name}{item.name}</Text> 
+                
+                {/* {item.name}{item.name}{item.name}{item.name}{item.name}{item.name} */}
+
                 <View style={styles.priceContainer}><Text style={styles.productName}>{item.price}</Text>
                 {item.oldPrice && (<Text style={styles.oldPrice}>{`(${item.oldPrice})`}</Text>)}
                 </View>
+            </View>
+            <View style={styles.buttonsContainer}>
+                <TouchableOpacity style={styles.favButton} onPress={ () => onAddToWishListPressed(item)}/>
+                <TouchableOpacity style={styles.starButton} onPress={ () => onRemoveFromWishListPressed(item)}/>
             </View>
         </View>)
     });
@@ -88,11 +104,29 @@ const styles = StyleSheet.create({
         marginLeft: 5
     },
     textContainer: {
-        flex: 0.5,
-        marginLeft: 16,
+        flexGrow: 1,
+        width: '60%',
+        backgroundColor: 'red',
+        marginHorizontal: 16,
+        flexGrow: 1,
         alignItems: 'flex-start',
-        flexDirection: 'column'
-        
+        flexDirection: 'column'    
+    },
+    starButton: {
+        backgroundColor: 'blue',
+        height: 20,
+        width: 20   
+    },
+    favButton: {
+        backgroundColor: 'gold',
+        height: 20,
+        width: 20   
+    },
+    buttonsContainer: {
+        width: 40,
+        flexDirection: 'column',
+        alignSelf: 'flex-end'
+
     },
     line: {
         height: 60,
